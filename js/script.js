d25a658c68d412ac84dd1d1a8a72befb3959cd4b
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-07-05 16:44:49 +0200

    Inizializzazione

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const { createApp } = Vue ;
+
+createApp({
+    data(){
+        return{
+            array : [ ] ,
+        }
+    },
+    created(){
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                const result=response.data;
+                this.array.push(
+                   
+                    result.response
+
+                )
+            })
+            
+        }
+    }
+}).mount('#app');
+
+
+// axios
+//     .get('https:{//flynn.boolean.careers/exercises/api/random/mail')
+//     .then(function(response){
+//         console.log('RESPONSE' , response );
+//         const result=response.data;
+//         console.log('RESULT' , result );
+
+// });
+
+// created(){
+//     for (let index = 0; index < array.length; index++) {
+//         axios
+//         .get('https:{//flynn.boolean.careers/exercises/api/random/mail')
+//         .then((response) => {
+//             this.arrayAxios.push({
+//                 text : response.data.response,
+//                 done: false ,
+
+//             })
+//         })
